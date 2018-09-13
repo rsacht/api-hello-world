31cb755cf66e83b22a7d5b8ed50fbcc928e21b16
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-09-12 21:29:42 -0300

    Criando o servidor

--- hello-node.js
+++ hello-node.js
@@ -1,2 +1,12 @@
 //Importando o módulo Http do Node.js para escutar as chamadas na porta 8180
 var http = require('http');
+
+http.createServer((req, res) =>{
+    res.writeHead(200, {
+        'Content-Type':'text/plain'
+    });
+    res.end('Node.js: Olá!');
+    console.log('Olá, manipulador solicitado');
+}).listen(8180, '127.0.0.1', ()=>{
+    console.log('Servidor Http Node.js rodando em http://127.0.0.1:8180');
+});
